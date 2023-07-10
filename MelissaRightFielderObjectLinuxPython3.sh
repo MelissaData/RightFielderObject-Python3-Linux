99ba3cc492d173b5f41948a0be68f60017781cc0
Melissa <>
2023-07-10 10:40:41 -0700

    Tested successfully using Melissa data files for 2023.06

--- MelissaRightFielderObjectLinuxPython3.sh
+++ MelissaRightFielderObjectLinuxPython3.sh
@@ -43,7 +43,7 @@ done
 
 
 # ######################### Config ###########################
-RELEASE_VERSION='2023.05'
+RELEASE_VERSION='2023.06'
 ProductName="RF_DATA"
 
 # Uses the location of the .sh file 
